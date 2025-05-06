066a6a291b415b5bf83538281199bed252541072
melkijo <jonathanandaraa@gmail.com>
2025-05-06 15:33:07 +0700

    chore: file structures

---
+++ curhat/Features/Awareness/View/AwarenessView.swift
@@ -0,0 +1,18 @@
+//
+//  AwarenessView.swift
+//  curhat
+//
+//  Created by Melki Jonathan Andara on 06/05/25.
+//
+
+import SwiftUI
+
+struct AwarenessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AwarenessView()
+}
