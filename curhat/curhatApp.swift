6d5a5055b00b6fbc94d29c023cb44abd9df9b990
melkijo <jonathanandaraa@gmail.com>
2025-05-06 15:06:35 +0700

    Initial Commit

---
+++ curhat/curhatApp.swift
@@ -0,0 +1,17 @@
+//
+//  curhatApp.swift
+//  curhat
+//
+//  Created by Melki Jonathan Andara on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct curhatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ curhatTests/curhatTests.swift
@@ -0,0 +1,17 @@
+//
+//  curhatTests.swift
+//  curhatTests
+//
+//  Created by Melki Jonathan Andara on 06/05/25.
+//
+
+import Testing
+@testable import curhat
+
+struct curhatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
