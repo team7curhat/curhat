f8e10ef12f1521e53500314a301deb2e18a5b944
Muhammad Ferial Ishakh <muhammadferialishakh@gmail.com>
2025-05-09 01:11:41 +0700

    feat: onboarding 1 2 3 to hifid

--- curhat/Features/Onboarding/View/onboarding1.swift
+++ curhat/Features/Onboarding/View/onboarding1.swift
@@ -1,6 +1,6 @@
 //
 //  Untitled.swift
-//  Kalkulator Cicilan Rumah
+//  curhat
 //
 //  Created by Muhammad Ferial Ishakh on 07/05/25.
 //
@@ -11,45 +11,60 @@ import SwiftUI
 struct onboarding1: View {
     var body: some View {
         NavigationStack{
-            VStack(spacing: 20) {
-                // Title
-                VStack(spacing: 20){
-                    Text("Selamat Datang")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                    Text("di EMOCHI")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                }
-                VStack(){
-                    Text("Ruang Aman Tuk")
-                        .font(.system(.title2, design: .rounded))
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                    Text("Melepas Beban")
-                        .font(.system(.title2, design: .rounded))
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
+            
+            ZStack(alignment: .bottom){
+                Image("onboarding1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
+                VStack(spacing: 20) {
                     Spacer()
+                    Spacer()
+                    Spacer()
+                    // Title
+                    VStack(spacing: 20){
+                        Text("Selamat Datang")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        Text("di EMOCHI")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                    }
+                    VStack(){
+                        Text("Ruang Aman Tuk")
+                            .font(.system(.title2, design: .rounded))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        Text("Melepas Beban")
+                            .font(.system(.title2, design: .rounded))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        Spacer()
+                    }
+//===================================  Button Gambar  ====================================
                 }
-                
-                //Character
-                
-                // Button
-                NavigationLink (destination: onboarding2()){
-                        Text("Ketuk gigiku untuk memulai.")
-                            
+                VStack{
+                    NavigationLink (destination: onboarding2()){
+                        Image("Gigi")
+                            .padding(.bottom, 85)
+                    }
+                    VStack{
+                        Text("Ketuk gigiku untuk memulai")
+                            .font(.system(.headline, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 60)
                     }
                 }
-                
+                .safeAreaPadding()
             }
-            .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
         }
     }
-
+}
 struct onboarding1_Previews: PreviewProvider {
     static var previews: some View {
         onboarding1()
--- curhat/Features/Onboarding/View/onboarding3.swift
+++ curhat/Features/Onboarding/View/onboarding3.swift
@@ -1,6 +1,6 @@
 
 //  onboarding 2.swift
-//  Kalkulator Cicilan Rumah
+//  curhat
 //
 //  Created by Muhammad Ferial Ishakh on 07/05/25.
 //
@@ -8,71 +8,74 @@
 
 import SwiftUI
 
-
 struct onboarding3: View {
     
-    @State private var nickname = ""
-    
+    @AppStorage("nickname") private var nickname = ""
     
     var body: some View {
         NavigationStack{
-            VStack(spacing: 20) {
-                
-                // Title
-                VStack(spacing: 1){
-                    Text("Ayo kita kenalan!")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                        .padding(.top, -150)
-                }
-                VStack{
-                    VStack(spacing: 15){
-                        Text("Namaku Ochi!")
-                            .font(.system(.title2, design: .rounded))
+            ZStack(alignment: .bottom){
+                Image("onboarding3")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
+                VStack(spacing: 20) {
+                    Spacer()
+                    // Title
+                    VStack{
+                        Text("Ayo kita kenalan!")
+                            .font(.system(.title, design: .rounded))
+                            .foregroundStyle(.primary10)
                             .fontWeight(.bold)
                             .multilineTextAlignment(.center)
-                        Text("Kamu lebih nyaman dipanggil siapa?")
-                            .font(.system(.title3, design: .rounded))
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
-                    }
-                    VStack {
-                        TextField("Masukkan namamu di sini", text: $nickname)
-                            .padding(12)
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-                            .frame(width: 347)
-                        
-                    }
-                    
-                    
-                    //Character
-                    
-                    // Button
-                    NavigationLink (destination: onboarding1()){
-                            Text("OK!")
-                                .font(.system(.title, design: .rounded))
-                                .fontWeight(.bold)
-                                .foregroundStyle(.white)
-                                .frame(width:100)
-                                .padding(10)
-                                .background(Color.gray)
-                                .cornerRadius(15)
+                            .padding(.top,-380)
+                        VStack{
+                            VStack(spacing: 15){
+                                Text("Namaku Ochi!")
+                                    .font(.system(.title2, design: .rounded))
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.bold)
+                                    .multilineTextAlignment(.center)
+                                Text("Kamu lebih nyaman dipanggil siapa?")
+                                    .font(.system(.body, design: .rounded))
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.bold)
+                                    .multilineTextAlignment(.center)
+                            }
+                            VStack {
+                                TextField("Masukkan namamu di sini", text: $nickname)
+                                    .padding(12)
+                                    .background(Color(.systemGray6))
+                                    .cornerRadius(8)
+                                    .frame(width: 347)
+                            }
+                            // Button
+                            VStack{
+                                NavigationLink (destination: onboarding1()){
+                                    Text("OK!")
+                                        .font(.system(.title, design: .rounded))
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(.white)
+                                        .frame(width:100)
+                                        .padding(10)
+                                        .background(Color.primary8)
+                                        .cornerRadius(15)
+                                }
+                            }
+                            .safeAreaPadding()
                         }
+                        .padding(.bottom, 200)
                     }
                 }
             }
-            .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
         }
     }
     
-struct onboarding3_Previews: PreviewProvider {
-    static var previews: some View {
-        onboarding3()
+    struct onboarding3_Previews: PreviewProvider {
+        static var previews: some View {
+            onboarding3()
         }
     }
-
-
-
+}
