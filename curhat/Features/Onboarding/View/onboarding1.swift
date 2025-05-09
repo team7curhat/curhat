6011e34d4a1b1d540cb62caabcfb13f547a68f24
melkijo <jonathanandaraa@gmail.com>
2025-05-09 16:45:32 +0700

    fix: user validation in onboarding

--- curhat/Features/Onboarding/View/onboarding1.swift
+++ curhat/Features/Onboarding/View/onboarding1.swift
@@ -9,58 +9,64 @@ import SwiftUI
 
 
 struct onboarding1: View {
+    @AppStorage("userNickname") private var nickname = ""
+    
     var body: some View {
-        NavigationStack{
-            ZStack(alignment: .bottom){
-                Image("onboarding1")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(maxWidth: .infinity)
-                VStack(spacing: 20) {
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    // Title
-                    VStack(spacing: 20){
-                        Text("Selamat Datang")
-                                                    .font(.system(.title, design: .rounded))
-                                                    .fontWeight(.bold)
-                                                    .multilineTextAlignment(.center)
-                        Text("di EMOCHI")
-                                                    .font(.system(.title, design: .rounded))
-                                                    .fontWeight(.bold)
-                                                    .multilineTextAlignment(.center)
-                    }
-                    VStack(){
-                        Text("Ruang Aman Tuk")
-                                                    .font(.system(.title2, design: .rounded))
-                                                    .fontWeight(.bold)
-                                                    .multilineTextAlignment(.center)
-                        Text("Melepas Beban")
-                                                    .font(.system(.title2, design: .rounded))
-                                                    .fontWeight(.bold)
-                                                    .multilineTextAlignment(.center)
+        if (!nickname.isEmpty){
+            HomeView()
+        }else{
+            NavigationStack{
+                ZStack(alignment: .bottom){
+                    Image("onboarding1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                    VStack(spacing: 20) {
                         Spacer()
+                        Spacer()
+                        Spacer()
+                        // Title
+                        VStack(spacing: 20){
+                            Text("Selamat Datang")
+                                .font(.system(.title, design: .rounded))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                            Text("di EMOCHI")
+                                .font(.system(.title, design: .rounded))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                        }
+                        VStack(){
+                            Text("Ruang Aman Tuk")
+                                .font(.system(.title2, design: .rounded))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                            Text("Melepas Beban")
+                                .font(.system(.title2, design: .rounded))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                        }
+                        //===================================  Button Gambar  ====================================
                     }
-                    //===================================  Button Gambar  ====================================
-                }
-                VStack{
-                    NavigationLink (destination: onboarding2().navigationBarBackButtonHidden(true)){
-                        Image("Gigi")
-                                                    .padding(.bottom, 85)
+                    VStack{
+                        NavigationLink (destination: onboarding2().navigationBarBackButtonHidden(true)){
+                            Image("Gigi")
+                                .padding(.bottom, 85)
+                        }
+                        VStack{
+                            Text("Ketuk gigiku untuk memulai")
+                                .font(.system(.headline, design: .rounded))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding(.bottom, 60)
+                        }
                     }
-VStack{
-Text("Ketuk gigiku untuk memulai")
-                                                    .font(.system(.headline, design: .rounded))
-                                                    .fontWeight(.bold)
-                                                    .foregroundColor(.white)
-                                                    .padding(.bottom, 60)
-}
+                    .safeAreaPadding()
                 }
-                                .safeAreaPadding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
             }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .ignoresSafeArea()
         }
     }
 }
